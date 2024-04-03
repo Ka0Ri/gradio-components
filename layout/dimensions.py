ed7e00cf5316085d230b15250c30da5ab72d2dd4
Ka0Ri <dtvu1707@gmail.com>
2024-04-03 18:13:52 +0900

    add layout

---
+++ layout/dimensions.py
@@ -0,0 +1,13 @@
+import gradio as gr
+
+css = """
+.container {
+    height: 100vh;
+}
+"""
+
+with gr.Blocks(css=css) as demo:
+    with gr.Column(elem_classes=["container"]):
+        name = gr.Chatbot(value=[["1", "2"]], height="70%")
+
+demo.launch()
